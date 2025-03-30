aa848c250482f6f99232bbe141994fafbb120a68
Anton Polishchuk <hardixxxxx@gmail.com>
2025-03-30 22:32:42 +0200

    add movie form validation and submission logic

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,30 +1,155 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
+import { Movie } from '../../types/Movie';
 
-export const NewMovie = () => {
-  // Increase the count after successful form submission
-  // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+type FormErrors = Partial<Record<keyof Movie, string>>;
+
+const validate = (values: Movie): FormErrors => {
+  const pattern =
+    /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/; // eslint-disable-line max-len
+
+  const { title, imgUrl, imdbUrl, imdbId } = values;
+  const errors: FormErrors = {};
+
+  if (!title) {
+    errors.title = 'Title is required';
+  }
+
+  if (!imgUrl) {
+    errors.imgUrl = 'ImgUrl is required';
+  }
+
+  if (!errors.imgUrl && !pattern.test(imgUrl)) {
+    errors.imgUrl = 'ImgUrl must be a link';
+  }
+
+  if (!imdbUrl) {
+    errors.imdbUrl = 'ImdbUrl is required';
+  }
+
+  if (!errors.imdbUrl && !pattern.test(imdbUrl)) {
+    errors.imdbUrl = 'ImdbUrl must be a link';
+  }
+
+  if (!imdbId) {
+    errors.imdbId = 'imdbId is required';
+  }
+
+  return errors;
+};
+
+const defaultValues: Movie = {
+  title: '',
+  description: '',
+  imgUrl: '',
+  imdbUrl: '',
+  imdbId: '',
+};
+
+type Props = {
+  onAdd: (movie: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
+  const [formKey, setFormKey] = useState(0);
+
+  const [values, setValues] = useState(defaultValues);
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  const handlerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    const updatedValues = {
+      ...values,
+      [name]: value,
+    };
+
+    setValues(updatedValues);
+
+    setErrors(currentErrors => {
+      const copy = { ...currentErrors };
+
+      delete copy[name as keyof Movie];
+
+      return copy;
+    });
+
+    const newError = validate(updatedValues);
+
+    if (Object.keys(newError).length > 0) {
+      setIsButtonDisabled(true);
+    } else {
+      setIsButtonDisabled(false);
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { title, description, imgUrl, imdbUrl, imdbId } = values;
+
+    // const newErrors = validate(values);
+
+    // setErrors(newErrors);
+    if (Object.keys(errors).length > 0) {
+      return;
+    }
+
+    onAdd({
+      title: title.trim(),
+      description: description.trim(),
+      imgUrl: imgUrl.trim(),
+      imdbUrl: imdbUrl.trim(),
+      imdbId: imdbId.trim(),
+    });
+    setValues(defaultValues);
+    setFormKey(prevKey => prevKey + 1);
+    setIsButtonDisabled(true);
+  };
 
   return (
-    <form className="NewMovie" key={count}>
+    <form className="NewMovie" key={formKey} onSubmit={handleSubmit}>
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={values.title}
+        onChange={handlerChange}
         required
       />
 
-      <TextField name="description" label="Description" value="" />
+      <TextField
+        name="description"
+        label="Description"
+        value={values.description}
+        onChange={handlerChange}
+      />
 
-      <TextField name="imgUrl" label="Image URL" value="" />
+      <TextField
+        name="imgUrl"
+        label="Image URL"
+        value={values.imgUrl}
+        onChange={handlerChange}
+        required
+      />
 
-      <TextField name="imdbUrl" label="Imdb URL" value="" />
+      <TextField
+        name="imdbUrl"
+        label="Imdb URL"
+        value={values.imdbUrl}
+        onChange={handlerChange}
+        required
+      />
 
-      <TextField name="imdbId" label="Imdb ID" value="" />
+      <TextField
+        name="imdbId"
+        label="Imdb ID"
+        value={values.imdbId}
+        onChange={handlerChange}
+        required
+      />
 
       <div className="field is-grouped">
         <div className="control">
@@ -32,6 +157,7 @@ export const NewMovie = () => {
             type="submit"
             data-cy="submit-button"
             className="button is-link"
+            disabled={isButtonDisabled}
           >
             Add
           </button>
